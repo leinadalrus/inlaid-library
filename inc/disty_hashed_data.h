9847569fb83a25695b6eb3acfc2d2fbebb19bf42
David <daniel.david.surla@gmail.com>
2023-04-16 07:04:27 +0100

    Init first commit

---
+++ inc/disty_hashed_data.h
@@ -0,0 +1,39 @@
+#ifndef DISTY_HASHED_DATA_H
+#define DISTY_HASHED_DATA_H
+
+enum EHashes
+{
+  Node = 0,
+};
+
+typedef struct UnitEntity
+{
+} UnitEntity;
+
+typedef struct Username
+{
+  const char *username[sizeof(15)];
+} Username;
+
+typedef struct Vector2Position
+{
+} Vector2Position;
+
+typedef struct DistyData
+{
+  char *source;
+  char *destination;
+  char *data_size;
+  char *user_data;
+} DistyData;
+
+typedef struct TrackingTable
+{
+  EHashes e_hash;
+  UnitEntity entity;
+  Username name;
+  Vector2Position vector2_position;
+  DistyData disty_data;
+} TrackingTable;
+
+#endif // DISTY_HASHED_DATA_H
