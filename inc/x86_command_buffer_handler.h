f5c4a26ffb81d06b4903a49d4a8a55d315a9d225
David <daniel.david.surla@gmail.com>
2023-04-27 07:48:15 +0100

    Update x86_command_buffer_handler.h

--- inc/x86_command_buffer_handler.h
+++ inc/x86_command_buffer_handler.h
@@ -1,4 +1,48 @@
 #ifndef x86_COMMAND_BUFFER_HANDLER_H
 #define x86_COMMAND_BUFFER_HANDLER_H
 
-#endif
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct PhantomMarker {
+  const char *token_string;
+  signed int cursor_position;
+  signed int read_position;
+  char *character_byte;
+} PhantomMarker;
+
+// `handle_context` of singleton strategy function pointers/contexts
+const int handle_context(char *destination, char *source, uint8_t data_size,
+                         char *user_data);
+int read_handled_context_argument(int (*interface_handle_callback)(),
+                                  char *user_data_header,
+                                  signed int status_flag, uint64_t data_size);
+
+typedef struct LintCommandBufferTree {
+  const char *p_buffers[];
+} LintCommandBufferTree;
+
+typedef struct LintCommandHandler {
+  LintCommandBufferTree *lint_command_buffer_tree;
+} LintCommandHandler;
+
+typedef struct LintCommandContext {
+  void *lint_command_context;
+} LintCommandContext;
+
+typedef struct LintServiceLocator {
+  LintCommandBufferTree *lint_command_buffer_tree;
+  LintCommandHandler *lint_command_handler;
+} LintServiceLocator;
+
+const int defer_context(LintCommandContext *lint_command_context);
+int handled_deferred_context(int (*defer_context_callback)(), void *user_data,
+                             void *iomem_source, void *table_data,
+                             uint64_t data_size);
+
+typedef struct LintAnnulService {
+  LintCommandBufferTree *lint_command_buffer_tree;
+  LintCommandHandler *lint_command_handler;
+} LintAnnulService;
+
+#endif // x86_COMMAND_BUFFER_HANDLER_H
