de92d666d0220f977c2cc5e77f7e0b680afd1f3a
David <daniel.david.surla@gmail.com>
2023-05-19 09:15:24 +0100

    Update amd64_orcv2jit_buildmodule.c

--- src/amd64_orcv2jit_buildmodule.c
+++ src/amd64_orcv2jit_buildmodule.c
@@ -5,9 +5,22 @@
 #define ASSERT_VARGS(_str, ...) // redefined for re-use
 #endif                          // !ASSERT_VARGS
 
-#if defined(ORCV2JIT_X86) || defined(ORCV2JIT_X64)
+#ifndef ORCV2JIT_X86
+#define ORCV2JIT_X86
 
-const char *TEST_CODES;
+enum TestCodes {
+  C3 = 0xC3,
+  CB = 0xCB,
+  C2 = 0xC2,
+  CA = 0xCA,
+  EB = 0xEB,
+  E9 = 0xE9,
+  EA = 0xEA,
+  FF = 0xff,
+  TWENTY_FIVE = 0x25,
+};
+
+const char *ENTRY_CODES;
 
 void memory_dump_fragment(char *placeback_data, long callback_data,
                           long callback_space) {
@@ -32,4 +45,28 @@ inline const int *maligned_vargs_exec(int argc, char *restrict argv[]) {
   return 0;
 }
 
+const int terminate_pageblock_source(const char *restrict pageblock_source) {
+  const int n0 = pageblock_source[0];
+  const int n1 = pageblock_source[1];
+
+  enum TestCodes test_code;
+  switch (test_code) {
+  case C3:
+  case CB:
+  case C2:
+  case EB:
+  case E9:
+  case EA:
+  case FF:
+  case TWENTY_FIVE:
+  default:
+    return 1;
+  }
+
+  return 0;
+}
+
+#endif // !ORCV2JIT_X86
+
+#if defined(ORCV2JIT_X86) || defined(ORCV2JIT_X64)
 #endif // !ORCV2JIT_X86 || ORCV2JIT_X64
