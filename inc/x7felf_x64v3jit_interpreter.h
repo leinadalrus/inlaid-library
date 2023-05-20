1ebd6e05df0c508accbe7503c93cb455ec71f8ae
David <daniel.david.surla@gmail.com>
2023-05-20 02:19:47 +0100

    added inc\x7felf_x64v3jit_interpreter.h

---
+++ inc/x7felf_x64v3jit_interpreter.h
@@ -0,0 +1,64 @@
+#ifndef X7FELF_X64V3JIT_INTERPRETER_H
+#define X7FELF_X64V3JIT_INTERPRETER_H
+
+#include "function_offload_phantom_marker.h"
+#include "spe_function_offload_hook.h"
+#include <stdint.h>
+
+#ifdef AMD64_ORC_JIT_INTERPRETER_H
+extern int amd64_x64v3_jit_interpreter_toggle;
+#endif
+
+typedef struct MemoryRegion {
+  uint8_t memory;
+  uint32_t mask;
+} MemoryRegion;
+
+typedef struct X64V3InternalClock {
+  uint32_t processing_unit_clock_address;
+} X64V3InternalClock;
+
+typedef struct X64V3RegionBundle {
+  MemoryRegion *memory_region;
+  X64V3InternalClock x64v3_internal_clock;
+} X64V3RegionBundle;
+
+void load_maked_coreboot_bios(int(*unix_nan_boxing));
+int get_x64v3_memory_region(uint32_t address, int is_written,
+                            MemoryRegion *memory_region);
+
+uint32_t
+get_processing_unit_controller(uint32_t processing_unit_controller_address);
+uint64_t get_internal_system_clock_cycles(int n_cycles);
+uint64_t set_x64v3_memory_region_timings(uint32_t starting_address,
+                                         uint32_t ending_address,
+                                         uint32_t memory_region, int bus_width,
+                                         int non_sequential, int sequential);
+void run_internal_system_clock_cycler(uint32_t processing_unit_clock_address);
+
+typedef struct X64V3Jit {
+} X64V3Jit;
+
+typedef struct X64V3JitAdapter {
+} X64V3JitAdapter;
+
+typedef struct X64V3JitInterpreter {
+  FunctionOffloadPhantomMarker *phantom_marker;
+} X64V3JitInterpreter;
+
+typedef struct X64V3DataBundle {
+  X64V3Jit *jit;
+  X64V3JitAdapter *adapter;
+  X64V3JitInterpreter *interpreter;
+} X64V3DataBundle;
+
+int x64v3_jit_startup_sentinel();
+
+X64V3JitInterpreter *
+initialise_external_jit_interpreter(X64V3JitInterpreter *interpreter);
+void deinitialise_external_jit_interpreter(X64V3JitInterpreter *interpreter);
+void reset_external_jit_interpreter(X64V3JitInterpreter *interpreter);
+void start_external_jit_interpreter(X64V3JitInterpreter *interpreter);
+void stop_external_jit_interpreter(X64V3JitInterpreter *interpreter);
+
+#endif // !X7FELF_X64V3JIT_INTERPRETER_H
---
+++ src/x7felf_x64v3jit_interpreter.c
@@ -0,0 +1,109 @@
+#include "../inc/x7felf_x64v3jit_interpreter.h"
+#include "../inc/amd64_command_ring_buffer.h"
+// need to have this include file separated
+// and outside of include guards
+
+#ifndef AMD64_ORC_JIT_INTERPRETER_H
+extern int amd64_x64v3_jit_interpreter_toggle;
+#endif
+
+#include <assert.h>
+#include <memory.h>
+#include <stdint.h>
+#include <string.h>
+
+void load_maked_coreboot_bios(int(*unix_nan_boxing)) {
+  switch (*unix_nan_boxing) {
+  default:
+    break;
+  }
+}
+
+int get_x64v3_memory_region(uint32_t address, int is_written,
+                            MemoryRegion *memory_region) {
+  return 0;
+}
+
+uint32_t
+get_processing_unit_controller(uint32_t processing_unit_controller_address) {
+  return 0;
+}
+
+uint64_t get_internal_system_clock_cycles(int n_cycles) { return 0; }
+
+uint64_t set_x64v3_memory_region_timings(uint32_t starting_address,
+                                         uint32_t ending_address,
+                                         uint32_t memory_region, int bus_width,
+                                         int non_sequential, int sequential) {
+  return 0;
+}
+
+void run_internal_system_clock_cycler(uint32_t processing_unit_clock_address) {}
+
+int x64v3_jit_runtime_sentinel() {
+  enum RingBufferStatusCodes rb_status_code;
+
+  switch (rb_status_code) {
+  case INVALID_ADDRESS:
+  case INVALID_ASID:
+  case INVALID_CERTIFICATE:
+  case INVALID_COMMAND:
+  case INVALID_CONFIG:
+  case INVALID_LENGTH:
+  case INVALID_PARAM:
+  case INVALID_PLATFORM_STATE:
+  default:
+    break;
+  }
+
+  return 0;
+}
+
+int x64v3_jit_startup_sentinel() {
+  switch (amd64_x64v3_jit_interpreter_toggle) {
+  case 0:
+    x64v3_jit_runtime_sentinel();
+  case 1:
+  default:
+    break;
+  }
+
+  return 0;
+}
+
+X64V3JitInterpreter *
+initialise_external_jit_interpreter(X64V3JitInterpreter *interpreter) {
+  X64V3DataBundle *x64v3_data_bundle;
+  ArenaState *arena_state;
+  MemoryRegion *memory_region;
+
+  uint8_t destination = (uint8_t)arena_state->arena_data.destination;
+  // arena_data->destination is mutable:
+  destination = destination >> memory_region->memory;
+  // arena_data->table_data is the bitmask for bit-ops
+  uint32_t table = (uint32_t)arena_state->arena_data.table_data;
+  table = table >> memory_region->mask;
+
+  return interpreter;
+}
+
+void deinitialise_external_jit_interpreter(X64V3JitInterpreter *interpreter) {
+  free(interpreter);
+  interpreter = NULL;
+  interpreter = initialise_external_jit_interpreter(interpreter);
+  X64V3RegionBundle *x64v3_region;
+  X64V3JitInterpreter *new_interpreter =
+      memset(interpreter, x64v3_region->memory_region->memory,
+             sizeof(X64V3JitInterpreter));
+  interpreter = new_interpreter;
+}
+
+void reset_external_jit_interpreter(X64V3JitInterpreter *interpreter) {
+  free(interpreter);
+  interpreter = NULL;
+  x64v3_jit_startup_sentinel();
+}
+
+void start_external_jit_interpreter(X64V3JitInterpreter *interpreter) {}
+
+void stop_external_jit_interpreter(X64V3JitInterpreter *interpreter) {}
