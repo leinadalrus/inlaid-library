0a098ab24700d0d32c2af77b626087316721bb53
David <daniel.david.surla@gmail.com>
2023-06-17 08:37:19 +0100

    Create interpreter.c

---
+++ src/interpreter.c
@@ -0,0 +1,70 @@
+#if ((defined(_M_X64) || defined(__amd64__)) != X64) || (defined(_WIN32) != WIN)
+#pragma error                                                                  \
+    "Wrong DynASM flags used: pass `-D X64` and/or `-D WIN` to dynasm.lua as appropriate"
+#endif
+#include "../ext/LuaJIT-2.0.5/dynasm/dasm_proto.h"
+#include "../ext/LuaJIT-2.0.5/dynasm/dasm_x86.h"
+#include <stdio.h>
+#include <stdlib.h>
+#if _WIN32
+#include <Windows.h>
+#else
+#include <sys/mman.h>
+#if !defined(MAP_ANONYMOUS) && defined(MAP_ANON)
+#define MAP_ANONYMOUS MAP_ANON
+#endif
+#endif
+#define MAP_ANONYMOUS int
+
+// TODO: PPU Interpreter
+// into SPU Recompiler
+
+#define TAPE_SIZE 30000
+#define MAX_NESTING 100
+
+typedef struct DasmStateContainer {
+  unsigned int n;
+  unsigned int npc;
+  unsigned int nextpc;
+  unsigned int nloops;
+  unsigned int loops[MAX_NESTING];
+  dasm_State *dasm_state;
+} DasmStateContainer;
+
+typedef struct DasmStateEncodeLink {
+  size_t size;
+  void *buffer;
+} DasmStateEncodeLink;
+
+// ... we'll rename bf_interpret to bf_compile and change its type signature:
+// void mb_interpret(const char *program, mbstate_t *state);
+void (*mb_compile(const char *program))(mbstate_t *state); // function pointer
+
+void *link_and_encode(DasmStateEncodeLink s, dasm_State *d[]) {
+  dasm_link(d, &s.size);
+#ifdef _WIN32
+  s.buffer = VirtalAlloc(0, s.size, MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
+#else
+  s.buffer = mmap(0, s.size, PROT_READ | PROT_WRITE,
+                  MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+#endif
+  dasm_encode(d, s.buffer);
+#ifdef _WIN32
+  {
+    DWORD dwOld;
+    VirtualProtect(s.buffer, s.size, PAGE_EXECUTE_READ, &dwOld);
+  }
+#else
+  mprotect(buf, sz, PROT_READ | PROT_EXEC);
+#endif
+  return s.buffer;
+}
+
+// 1. Dynamically specialized CRuby instructions
+// 2. RTL code instructions
+// 3. Hybrid stack/RTL instructions
+// 4. Type-specialized instructions
+// 5. Lazy basic block versioning
+// 6. Profile-based specialization
+// 7. Specialized iterator instructions
+// 8. Dynamic flow of specialized instructions
