81114ce0ff1b667874f2a612acca4c5bd0727db7
David <daniel.david.surla@gmail.com>
2023-06-22 21:35:49 +0100

    added inc/inputs.h ...
    ...
    modified:
    * src/sections.h

--- src/sections.c
+++ src/sections.c
@@ -1,44 +1,65 @@
-#include "../inc/buffered_drawing_renders.h"
-#include "../inc/ordnance_service_locator.h"
-
-#define RESERVED_CURRENT_PROGRAM_REGISTER_SIZE 4
-#define SPRITE_TEXTURE_WIDTH 255
-#define TILEMAP_SIZE_MIN 16 // could represent the top half of the register
-#define PADDING_SIZE 3
-#define MARGIN_SIZE 4
-// the padding and margin are combined for texture culling
-// TODO(Daniel): create a single 'sect' like room, similar to the Build Engine
-typedef struct TilemapFloor {
-  Vector2 tiles;
-} TilemapFloor;
-
-typedef struct ConcaveBackdrop {
-  Image image;
-} ConcaveBackdrop;
-
-typedef struct SectVertexMatrix {
-  VertexPoints *vertex_point_matrices[RESERVED_CURRENT_PROGRAM_REGISTER_SIZE];
-} SectVertexMatrix;
+#include "../inc/inputs.h"
+#include <raylib.h>
 
 int create() {
   int boolean;
   TilemapFloor tilemap_floor;
   tilemap_floor.tiles.x = 8;
   tilemap_floor.tiles.y = 8;
+
+  tilemap_floor.id = (unsigned char *)calloc(
+      tilemap_floor.tiles.x * tilemap_floor.tiles.y, sizeof(unsigned char));
+
+  RenderTexture2D tile_occlusion =
+      LoadRenderTexture(tilemap_floor.tiles.x, tilemap_floor.tiles.y);
+  SetTextureFilter(tile_occlusion.texture, TEXTURE_FILTER_BILINEAR);
+
   return boolean;
 }
 
-int refer() {
+int refer(TilemapFloor floor) {
   int boolean = 0;
+  RenderTexture2D tile_occlusion =
+      LoadRenderTexture(floor.tiles.x, floor.tiles.y);
+  SetTextureFilter(tile_occlusion.texture, TEXTURE_FILTER_BILINEAR);
+
+  BeginTextureMode(tile_occlusion);
+  ClearBackground(BLANK);
+  for (int i = 0; i < floor.tiles.y; i++)
+    for (int j = 0; j < floor.tiles.x; j++)
+      if (floor.id[i * floor.tiles.x + j] == 0)
+        DrawRectangle(j, i, 1, 1, BLACK);
+      else if (floor.id[i * floor.tiles.x + j] == 2)
+        DrawRectangle(i, j, 1, 1, Fade(BLACK, .9f));
+  // NOTE(Daniel): Array subscript is not an integer[?]
+  EndTextureMode();
+
   return boolean;
 }
 
-int update() {
+int update(TilemapFloor floor, PlayerBundle player) {
   int boolean;
+
+  if (player.position.position.x < 0)
+    player.position.position.x = 0;
+  else if ((player.position.position.x +
+            (player.sprite.width * player.sprite.height)) >
+           (floor.tiles.x * TILEMAP_SIZE_MIN))
+    player.position.position.x = (float)floor.tiles.x * TILEMAP_SIZE_MIN -
+                                 (player.sprite.width * player.sprite.height);
+  if (player.position.position.y < 0)
+    player.position.position.y = 0;
+  else if ((player.position.position.y +
+            (player.sprite.width * player.sprite.height)) >
+           (floor.tiles.y * TILEMAP_SIZE_MIN))
+    player.position.position.y = (float)floor.tiles.y * TILEMAP_SIZE_MIN -
+                                 (player.sprite.width * player.sprite.height);
+
   return boolean;
 }
 
-int destroy() {
+int destroy(TilemapFloor floor) {
   int boolean;
+  free(floor.id);
   return boolean;
 }
